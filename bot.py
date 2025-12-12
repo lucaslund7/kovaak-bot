09dae1b249ad6230e0a8ace33d83e0ac943f887c
lucaslund7 <lucasclund@gmail.com>
2025-12-12 15:30:14 -0600

    temp solution for data storage through json file

--- bot.py
+++ bot.py
@@ -2,24 +2,57 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import os
+import json
+from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 client = commands.Bot(command_prefix=">", intents=intents)
 
+path = Path("data.json")
 token = os.getenv('TOKEN')
-
-guild = discord.Object(id=1391210266884309174)
 
 @client.event
 async def on_ready():
     print("Logged in as a bot {0.user}".format(client))
 
+@client.event
+async def on_guild_join(guild):
+    name = guild.name
+    id = str(guild.id)
+    print("Joined guild '" + name + "' Id: " + id)
+    if id not in data:
+        data[id] = {
+            "server_name": name,
+            "guild_id": id,
+            "links": {}
+        }
+        save_data()
+
+
 @client.command()
 async def link(ctx, arg):
     await ctx.send(arg)
 
+def check_data():
+    if path.exists():
+        print("Data file found")
+        try:
+            return json.loads(path.read_text(encoding="utf-8"))
+        except json.JSONDecodeError:
+            return {}
+    else:
+        print("Data file not found, created file")
+        path.write_text("{}", encoding="utf-8")
+        return {}
+
+def save_data():
+    path.write_text(json.dumps(data, indent=2), encoding="utf-8")
+    print("Saved data")
+
+data = check_data()
 client.run(token)
